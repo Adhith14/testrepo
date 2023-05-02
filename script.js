84a44899ff07b19812d66886bae9ea072c0c3e9d
Adhith <adhithkl2003@gmail.com>
2023-05-02 14:45:50 +0530

    js funtion added

--- script.js
+++ script.js
@@ -35,23 +35,63 @@
 // f.pop()
 // console.log(f);
 
-let a=[
-    {
-        id:1,
-        name:"aaa"
-    },
-    {
-        id:2,
-        name:"bbb"
-    },
-    {
-        id:3,
-        name:"ccc"
-    },
-    {
-        id:4,
-        name:"ddd"
-    }
-];
-console.log(a[1].id)
+// let a=[
+//     {
+//         id:1,
+//         name:"aaa"
+//     },
+//     {
+//         id:2,
+//         name:"bbb"
+//     },
+//     {
+//         id:3,
+//         name:"ccc"
+//     },
+//     {
+//         id:4,
+//         name:"ddd"
+//     }
+// ];
+// console.log(a[1].id)
+//  function add(a,b){
+//     console.log(a+b);
+//  }
+// add = (a,b) => {console.log(a+b)}
+// add(1,62);
 
+// add = (a, b) => {
+//     if (a + b > 10)
+//         console.log("number greater than 10")
+
+//     else
+//         console.log("not greater")
+// }
+// add(2, 1)
+
+
+// print=(a)=>{
+// for(var i=0;i<n.length;i++){
+//     console.log(n[i]);
+// }
+// }
+let n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// print(n)
+
+// print2=(a)=>{
+//     i=0
+//     while(i<a.length){
+//         console.log(a[i]);
+//         i++;
+//     }
+// }
+// print2(n)
+
+print3 = (a) => {
+    i = 0;
+    do {
+        console.log(n[i]);
+        i++;
+    } while (i < a.length);
+}
+print3(n);
