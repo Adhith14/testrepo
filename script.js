7475a6d8704314265b9e1f70ab4af6cbf41de3c1
Adhith <adhithkl2003@gmail.com>
2023-05-02 12:44:29 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// let a=10;
+// let b=250;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+// let car={
+//     model:"benz",
+//     color:"red",
+//     price:100
+// }
+
+// console.log(car);
+// console.log(car.color);
+// console.log(`my car is ${car.color} ${car.model}`);
+
+// let person={
+//     name:"adhith",
+//     collge:"cce",
+//     class:"cs",
+//     address:{
+//         housename:"xyz",
+//         street:"qwerty"
+//     }
+// }
+// console.log(person);
+
+// let f=["apple","mango","orange"]
+// console.log(f)
+// f.shift("aaaa");
+// console.log(f)
+// f.pop()
+// console.log(f);
+
+let a=[
+    {
+        id:1,
+        name:"aaa"
+    },
+    {
+        id:2,
+        name:"bbb"
+    },
+    {
+        id:3,
+        name:"ccc"
+    },
+    {
+        id:4,
+        name:"ddd"
+    }
+];
+console.log(a[1].id)
+
